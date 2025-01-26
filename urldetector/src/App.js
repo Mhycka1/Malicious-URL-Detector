169127d5008c7abd05d3735002f295bc5af65245
Mhycka1 <mmcdon10@terpmail.umd.edu>
2025-01-26 13:26:16 -0500

    small css changes

--- urldetector/src/App.js
+++ urldetector/src/App.js
@@ -4,20 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1 className="Title">Malicious URL Detector</h1>
       </header>
+      <main>
+        <h2>
+          This site analyzes URLs with an ML model trained on a dataset of malicious URLs
+          to determine whether it is safe to click or enter the site.
+        </h2>
+        <form>
+          <input type="text" id="url" name="url" placeholder="Enter URL here..." />
+        </form>
+        <form>
+          <button className="button"> Start url analysis</button>
+        </form>
+        
+      </main>
     </div>
   );
 }
